93b46d94e44d205cb882f568d7c3a79a8ddfe166
Jason Dinh <dinhtq1@gmail.com>
2018-04-20 20:37:16 -0400

    intitialized express catch all route

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+const express = require('express');
 const Sequelize = require('sequelize');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const PORT = 3000;
+// Set up the express app
+const app = express();
+
+// Log requests to the console.
+app.use(logger('dev'));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Setup a default catch-all route that sends back a welcome message in JSON format.
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
 const sequelize = new Sequelize({
   database: 'todo',
   username: 'postgres',
   password: '',
   dialect: 'postgres'
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
 
 sequelize.authenticate()
@@ -14,3 +36,4 @@ sequelize.authenticate()
 .catch((err) => {
   console.log(err);
 });
+
